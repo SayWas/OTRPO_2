92ff271c6446cb44bd1c5c7cc9a241f237374c1e
Vladislav Saifulin <saifulinvladislav@gmail.com>
2023-12-13 02:32:07 +0500

    tests fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,28 +15,28 @@ from config import (DB_HOST_TEST, DB_NAME_TEST, DB_PASSWORD_TEST, DB_PORT_TEST, 
 from src import app, get_async_session, Base
 
 
-DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASSWORD_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
-
-engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
-async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
-Base.metadata.bind = engine_test
-
-
-async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
+# DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASSWORD_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
+#
+# engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
+# async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
+# Base.metadata.bind = engine_test
 
 
-app.dependency_overrides[get_async_session] = override_get_async_session
+# async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+#     async with async_session_maker() as session:
+#         yield session
 
 
-@pytest.fixture(autouse=True, scope='session')
-async def prepare_database():
-    async with engine_test.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-    async with engine_test.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+# app.dependency_overrides[get_async_session] = override_get_async_session
+
+
+# @pytest.fixture(autouse=True, scope='session')
+# async def prepare_database():
+#     async with engine_test.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+#     yield
+#     async with engine_test.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
 
 
 @pytest.fixture(scope='session')
