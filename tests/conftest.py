a96263b97cdd67abd7d54777f9b86fc5d1edcab0
Vladislav Saifulin <saifulinvladislav@gmail.com>
2023-12-13 03:02:52 +0500

    tests fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,11 +39,11 @@ async def prepare_database():
         await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest.fixture(scope='session')
-def event_loop(request):
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
+# @pytest.fixture(scope='session')
+# def event_loop(request):
+#     loop = asyncio.get_event_loop_policy().new_event_loop()
+#     yield loop
+#     loop.close()
 
 
 client = TestClient(app)
